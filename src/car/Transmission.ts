6bf4ef2744448d7b008e7566904f80496586433e
Aaaaaaa170 <hizagaitasugi@outlook.com>
2026-07-11 09:59:43 +0900

    Update Transmission.ts

--- src/car/Transmission.ts
+++ src/car/Transmission.ts
@@ -1,1 +1,46 @@
+export class Transmission {
 
+
+    gear:number = 1;
+
+
+    gears:number[] = [
+        0,
+        3.2,
+        2.1,
+        1.5,
+        1.2,
+        1.0,
+        0.8
+    ];
+
+
+
+    shiftUp(){
+
+        if(this.gear < 6)
+            this.gear++;
+
+    }
+
+
+
+    shiftDown(){
+
+        if(this.gear > 1)
+            this.gear--;
+
+    }
+
+
+
+    ratio(){
+
+        return this.gears[
+            this.gear
+        ];
+
+    }
+
+
+}
