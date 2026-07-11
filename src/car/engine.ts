cba62abcf5fbd3d4154039a2edad86ee601a2a06
Aaaaaaa170 <hizagaitasugi@outlook.com>
2026-07-11 10:01:11 +0900

    Update engine.ts

--- src/car/engine.ts
+++ src/car/engine.ts
@@ -1,23 +1,58 @@
 export class Engine {
 
-    power:number;
+    horsepower:number;
+
     rpm:number;
+
+    maxRPM:number;
 
 
     constructor(){
 
-        this.power = 200;
+        this.horsepower = 300;
+
         this.rpm = 1000;
+
+        this.maxRPM = 8000;
 
     }
 
 
-    update(speed:number){
+
+    update(
+        speed:number
+    ){
 
         this.rpm =
             1000 +
-            Math.abs(speed) * 300;
+            speed * 250;
+
+
+        if(this.rpm > this.maxRPM){
+
+            this.rpm =
+                this.maxRPM;
+
+        }
+
 
     }
+
+
+
+    power(){
+
+        const ratio =
+            this.rpm /
+            this.maxRPM;
+
+
+        return (
+            this.horsepower *
+            ratio
+        );
+
+    }
+
 
 }
