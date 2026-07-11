1a3310da6b510dec5ec0a0a2d79fe7bfd61c2765
Aaaaaaa170 <hizagaitasugi@outlook.com>
2026-07-11 09:59:24 +0900

    Update Wheel.ts

--- src/car/Wheel.ts
+++ src/car/Wheel.ts
@@ -1,1 +1,43 @@
+import * as THREE from "three";
 
+export class Wheel {
+
+    mesh: THREE.Mesh;
+
+
+    constructor(){
+
+        this.mesh =
+            new THREE.Mesh(
+
+                new THREE.CylinderGeometry(
+                    0.35,
+                    0.35,
+                    0.25,
+                    32
+                ),
+
+                new THREE.MeshStandardMaterial({
+                    color:0x111111
+                })
+
+            );
+
+
+        // タイヤを横向きにする
+
+        this.mesh.rotation.z =
+            Math.PI / 2;
+
+
+    }
+
+
+    rotate(speed:number){
+
+        this.mesh.rotation.x -=
+            speed * 0.1;
+
+    }
+
+}
